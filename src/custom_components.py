47bd3a4535f29bbd3ea7e321c6723337c062748a
Pavel Nedelnik <p.nedelnik@email.cz>
2022-11-04 13:22:14 +0100

    new layout

--- src/custom_components.py
+++ src/custom_components.py
@@ -2,45 +2,76 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import html
 from dash import dcc
+import dash_daq
 
-def make_map(name, title):
-    return  html.Div([
+# TODO transform into objects
+
+def place_in_container(component_body:list):
+    return html.Div([
         dbc.Card(
-            dbc.CardBody([
-                html.H4(title, className="card-title"),
-                dcc.Graph(
-                    id=name,
-                    config={
-                        'displayModeBar': False
-                    }
-                ) 
-            ])
-        ),  
+            dbc.CardBody(
+                component_body
+            ),
+            color = 'dark',
+        )
+    ])
+
+def make_plot(map_header, idx, figure={}):
+    return  place_in_container([
+        map_header,
+        dcc.Graph(
+            id=idx,
+            config={
+                'displayModeBar': False
+            },
+            figure=figure,
+        ),
     ])
 
 
-def make_dropdown(name, options):
-    return dbc.Card(
-        dbc.CardBody([
-            dbc.Row([
-                dbc.Col([
-                    dcc.Dropdown(
-                        options=options,
-                        value=options[0],
-                        id=name,
-                        className="dbc", # to be compatible with dbc themes (it's a dcc component)
-                    )
-                ])
-            ])
+def make_tooltip_title(title, idx, tooltip_text):
+    return html.Div(
+        [
+            html.H4(
+                [
+                    html.Span(
+                        title,
+                        id=idx,
+                        style={"textDecoration": "underline", "cursor": "pointer"},
+                    ),
+                ]
+            ),
+            dbc.Tooltip(
+                tooltip_text,
+                target=idx,
+            ),
+        ],
+        className="card-title"
+    )
+
+def make_toggle(label_false, label_true, idx):
+    return place_in_container(
+        dbc.Row([
+            dbc.Col([comp]) for comp in [label_false, dash_daq.BooleanSwitch(id=idx, className='dbc'), label_true] if not isinstance(comp, str) or len(comp)
         ])
     )
 
 
-def make_table():
-    table_header = [
-        html.Thead(html.Tr([html.Th("First Name"), html.Th("Last Name")]))
-    ]
+# TODO try adding a tooltip
+def make_dropdown(idx, options):
+    return place_in_container([
+        dcc.Dropdown(
+            options=options,
+            value=options[0],
+            id=idx,
+            className="dbc", # to be compatible with dbc themes (it's dcc)
+        )
+    ])
 
+
+# TODO table body
+def make_table(title, idx):
+    # table header
     row1 = html.Tr([html.Td("Arthur"), html.Td("Dent")])
     row2 = html.Tr([html.Td("Ford"), html.Td("Prefect")])
     row3 = html.Tr([html.Td("Zaphod"), html.Td("Beeblebrox")])
@@ -48,58 +79,9 @@ def make_table():
 
     table_body = [html.Tbody([row1, row2, row3, row4])]
 
-    table = dbc.Table(table_header + table_body, bordered=True)
-    return table
+    table = dbc.Table(table_body, bordered=True)
 
-
-def make_tooltip():
-    return html.Div(
-        [
-            html.P(
-                [
-                    "I wonder what ",
-                    html.Span(
-                        "floccinaucinihilipilification",
-                        id="tooltip-target",
-                        style={"textDecoration": "underline", "cursor": "pointer"},
-                    ),
-                    " means?",
-                ]
-            ),
-            dbc.Tooltip(
-                "Noun: rare, "
-                "the action or habit of estimating something as worthless.",
-                target="tooltip-target",
-            ),
-        ]
-    )
-
-
-def make_slider(calibration):
-    return  html.Div([
-        dbc.Card(
-            dbc.CardBody([
-                dcc.RangeSlider(
-                    min=calibration[0],
-                    max=calibration[-1],
-                    value=[calibration[0], calibration[-1]],
-                    id='wavelength_slider',
-                    tooltip={"placement": "bottom", "always_visible": True},
-                    step=.2,
-                    marks=None,
-                )
-            ])
-        ),  
-    ])
-
-
-def make_spectra(name, title):
-    return html.Div([
-        dbc.Card(
-            dbc.CardBody([
-                dcc.Graph(
-                    id=name,
-                ) 
-            ])
-        ),  
-    ])
+    return place_in_container([
+            make_tooltip_title(title, 'table_title', 'TBD'),
+            table,
+        ])
