f9502908f383db8548322ccc5d9fc002e3ace4e4
Pavel Nedelnik <p.nedelnik@email.cz>
2022-11-04 14:33:34 +0100

    fixed error

--- src/rowwise_metrics.py
+++ src/rowwise_metrics.py
@@ -17,7 +17,7 @@ def rowwise_cosine(y_true, y_pred):
     """
     https://stackoverflow.com/questions/49218285/cosine-similarity-between-matching-rows-in-numpy-ndarrays
     """
-    return 1 - np.einsum('ij,ij->i', y_true, y_pred) / (
+    return - np.einsum('ij,ij->i', y_true, y_pred) / (
                 np.linalg.norm(y_true, axis=1) * np.linalg.norm(y_pred, axis=1)
         )
   
